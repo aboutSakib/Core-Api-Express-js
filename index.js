9a67241f68804d431076be96db5a4520bbf715c5
Sakib Ahmed <sakibhasan94470@gmail.com>
2024-12-16 23:29:48 +0600

    sql database todos

--- index.js
+++ index.js
@@ -1,48 +1,94 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const mysql = require("mysql");
 const app = express();
 const port = 3000;
 app.use(bodyParser.json());
 
+// mysql connection
+const db = mysql.createConnection({
+  host: "localhost",
+  port: 3306,
+  user: "root",
+  password: "",
+  database: "todos",
+});
+db.connect((err) => {
+  if (err) {
+    console.log("Mysql connection error", err);
+    return;
+  }
+  console.log("Connected Mysql database Successfylly..");
+});
+
 //create todos
-let taskList = {};
+
 app.post("/todo", (req, res) => {
-  const id = Math.floor(Math.random() * 100);
-  let newTaskList = {
-    id: id,
-    task: req.body.task,
-    isCompleted: true,
-    CreateAt: new Date(),
-    updatedAt: new Date(),
-  };
-  taskList[id] = newTaskList;
-  res.send(newTaskList);
+  const sql = `INSERT INTO  todoapp (task,isCompleted) VALUES ("${req.body.task}", False)`;
+  db.query(sql, (err, result) => {
+    if (err) {
+      console.log("Error Inserting data", err);
+      res.status(5000).send({ Error: "Failed to create todo" });
+    } else {
+      const sql = `SELECT * FROM todoapp WHERE id=${result.insertId} LIMIT 1`;
+      db.query(sql, (err, result) => {
+        if (err) {
+          console.log("Error faching Data ", err);
+          res.status(500).send({ error: "Unable to Create todos" });
+        } else {
+          res.status(201).send(result);
+        }
+      });
+    }
+  });
 });
 
 // get task
 app.get("/todo", (req, res) => {
-  res.send(taskList);
+  const sql = ` SELECT * FROM todoapp`;
+  db.query(sql, (err, result) => {
+    if (err) {
+      console.log("Error feching todo", err);
+    } else {
+      res.send(result);
+    }
+  });
 });
 // updated TaskLis
 app.put("/todo/:id", (req, res) => {
-  let id = req.params.id;
-  let taskItem = taskList[id];
-  let updatedTaskList = {
-    id: taskItem.id,
-    task: req.body.task,
-    isCompleted: req.body.isCompleted,
-    CreateAt: new Date(),
-    updatedAt: req.body.updatedAt,
-  };
-  taskList[id] = updatedTaskList;
-  res.send(updatedTaskList);
+  const sql = `UPDATE todoapp
+  SET task='${req.body.task}', isCompleted=${req.body.isCompleted}
+  WHERE id=${req.params.id}`;
+
+  db.query(sql, (err, result) => {
+    if (err) {
+      console.log("DB Updated error", err);
+      res.status(500).send({ message: "Unable to Updated Item" });
+    } else {
+      const sql = `SELECT * FROM todoapp WHERE id=${req.params.id} LIMIT 1`;
+      db.query(sql, (err, result) => {
+        if (err) {
+          console.log("Error faching Data ", err);
+          res.status(500).send({ error: "Unable to Updated todos" });
+        } else {
+          res.status(201).send(result);
+        }
+      });
+    }
+  });
 });
 
 // deleted task
 app.delete("/todo/:id", (req, res) => {
-  let id = req.params.id;
-  delete taskList[id];
-  res.send({ Message: "Your Task already deleted", id: id });
+  const sql = ` SELECT * FROM todoapp WHERE ${req.params.id} `;
+  db.query(sql, (err, result) => {
+    if (err) {
+      console.log("Unable to deleted Item", err);
+      res.status(500).send({ error: "unable to deleted item" });
+    } else {
+      res.status(200).send({ message: "Successfylly deleted item" });
+    }
+  });
 });
 app.listen(port, () => {
   console.log(`server is running on ${port}`);
